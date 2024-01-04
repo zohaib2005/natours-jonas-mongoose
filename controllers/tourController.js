c5c12eade738b96fd09521b7d4e1a1377b6758f5
zohaib <zohaibshafiq2005@gmail.com>
2024-01-04 18:52:59 +0500

    advance filtering greater lessthan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,23 +2,32 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query, queryObj);
+
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    // {difficulty: 'easy', duration: {$gte: 5} }
+    // {difficulty: 'easy', duration: {gte: '5'} }
+    // gte, gt, lte, lt
 
-    const query = Tour.find(queryObj);
+    const query = Tour.find(queryStr);
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     res.status(200).json({
       status: 'success',

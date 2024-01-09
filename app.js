545cd59e2febad0b3ee49ef47958c651780c2093
zohaib <zohaibshafiq2005@gmail.com>
2024-01-09 17:28:14 +0500

    handle undefined routes

--- app.js
+++ app.js
@@ -14,15 +14,17 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the MiddleWare🐇');
-  next();
-});
-
 // 2) ROUTE HANDLERS
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;

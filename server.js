ec6a24d2a12f027c583e44a73d685b11f3380c8a
zohaib <zohaibshafiq2005@gmail.com>
2024-01-10 15:42:57 +0500

    errors outside express unhandled rejections

--- server.js
+++ server.js
@@ -14,9 +14,18 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(console.log('DB connection successful!'));
+  .then(console.log('DB connection successful!'))
+  .catch((err) => console.log('ERROR'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN!!!');
+  server.close(() => {
+    process.exit(1);
+  });
 });

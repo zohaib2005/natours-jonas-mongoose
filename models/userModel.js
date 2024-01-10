deac51d755ed3e26db53a720b08a43dde350ceae
zohaib <zohaibshafiq2005@gmail.com>
2024-01-10 20:28:35 +0500

    module 10 Authentication Authorization and Security, vid Modelling Users Schema

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'password must have more or equal than 10 characters '],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;

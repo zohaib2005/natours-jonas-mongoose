13ff94110b2f3a63c1c213c4c41de4cc028da87b
zohaib <zohaibshafiq2005@gmail.com>
2024-01-12 12:40:58 +0500

    protecting tour routes authorization part1

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
+});
+
 // 2) ROUTE HANDLERS
 
 // 3) ROUTES

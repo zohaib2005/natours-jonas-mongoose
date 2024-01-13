ff9e8b90e6d116940b48e75eaa3307ff31d62527
zohaib <zohaibshafiq2005@gmail.com>
2024-01-13 21:50:20 +0500

    authorization restrictTo function

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,3 +93,16 @@ exports.protect = catchAsync(async (req, res, next) => {
   ueq.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+
+    next();
+  };
+};

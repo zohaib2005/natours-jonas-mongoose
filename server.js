20460803221d5381c7591609fa703f79d55ddd44
zohaib <zohaibshafiq2005@gmail.com>
2024-01-15 18:16:59 +0500

    sending email nodemailer

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+  process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
@@ -22,18 +20,17 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(console.log('DB connection successful!'))
-  .catch((err) => console.log('ERROR'));
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION! SHUTTING DOWN!!!');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on('unhandledRejection', (err) => {
+//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });

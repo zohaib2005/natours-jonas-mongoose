6e36922c57a26e717d18c1ad758f789f60351092
zohaib <zohaibshafiq2005@gmail.com>
2024-05-31 10:12:17 +0500

    helmet middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,12 +11,15 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set Security HTTP Headers
+app.use(helmet());
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// rate limiting helps to prevent DOS attack and brute force attack
+// limiting requests from same API helps to prevent DOS attack and brute force attack
 // This will allow 100 requests in an hour
 const limiter = rateLimit({
   max: 100,
@@ -25,9 +29,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
